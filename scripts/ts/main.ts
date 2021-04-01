fd24acc881672cc5ec110f0b562824ec5d70c570
shangaro <dibasbaral007@gmail.com>
2021-04-01 11:50:23 -0400

    add prettier and eslint config

--- scripts/ts/main.ts
+++ scripts/ts/main.ts
@@ -1,19 +1,20 @@
-import template from 'lodash/template';
-import { Car } from './car';
-
-const outputElement = document.getElementById('output');
-var car= new Car("subaru");
+import template from "lodash/template";
+import { Car } from "./car";
+const outputElement = document.getElementById("output");
+const car = new Car("subaru");
 if (outputElement) {
-  const compiled = template(`
+    const compiled = template(
+        `
     <h1><%- heading %></h1>
     Current date and time: <%- dateTimeString %></br>
     Car Model:<%- carmodel%>
-  `.trim());
-  outputElement.innerHTML = compiled({
-    heading: 'ts-demo-webpack',
-    dateTimeString: new Date().toISOString(),
-    carmodel:car.printEngine()
-  });
+  `.trim()
+    );
+    outputElement.innerHTML = compiled({
+        heading: "ts-demo-webpack",
+        dateTimeString: new Date().toISOString(),
+        carmodel: car.printEngine(),
+    });
 
-car.getItem();
+    car.getItem();
 }
